801bdd653b2e19ee552e256e5fbac4455c726410
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-03-22 16:43:48 -0300

    create style for view login

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const Login: React.FC = () => {
   return (
-    <div>Login</div>
+    <Container className="mt-5">
+      <h2 className="text-center mb-4">Sign In 🚀</h2>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button className='mt-3' variant="primary" type="submit" >
+          Login
+        </Button>
+      </Form>
+      <p className="text-center mt-3">¿Dont have an account yet?
+      <br/>
+       <NavLink to=''> Create account 😄</NavLink></p>
+    </Container>
   )
 }
 
