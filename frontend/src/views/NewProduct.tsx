f1e47be53c71401d9a20a0b39210c9091e797b7a
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-04-01 15:54:54 -0300

    add funtion for view NewProduct

--- frontend/src/views/NewProduct.tsx
+++ frontend/src/views/NewProduct.tsx
@@ -1,59 +1,67 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
-
-// TODO: las funciones son a modo de ejemplo para no perderme, esto tiene que con typeScript
+import { ENDPOINT } from '../config/constans'
 
 const NewProduct: React.FC = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    img: '',
-    price: ''
-  })
+  const [newProduct, setNewProduct] = useState({ title: '', img: '', description: '', price: '' })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }))
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewProduct({ ...newProduct, [event.target.name]: event.target.value })
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // Aquí puedes enviar formData a tu backend para procesarlo
-    console.log(formData)
-    // Limpia el formulario después de enviar
-    setFormData({
-      title: '',
-      description: '',
-      img: '',
-      price: ''
-    })
+  const getData = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    try {
+      event.preventDefault()
+      const response = await axios.post(ENDPOINT.newproduct, newProduct)
+      window.alert('Producto ingresado con éxito 😀.')
+      console.log(response.data)
+    } catch (error) {
+    window.alert(`${error.response.data.message} 🙁.`)
+    }
   }
-
   return (
     <Container className="mt-5 text-info">
       <h2 className="text-center mb-4">New Product</h2>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={getData}>
         <Form.Group controlId="formTitle">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter title" name="title" value={formData.title} onChange={handleChange} />
+          <Form.Control
+          type="text" placeholder="Enter title"
+          name="title"
+          value={newProduct.title}
+          onChange={handleChange} />
         </Form.Group>
 
         <Form.Group controlId="formDescription">
           <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Enter description" name="description" value={formData.description} onChange={handleChange} />
+          <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Enter description"
+          name="description"
+          value={newProduct.description}
+          onChange={handleChange} />
         </Form.Group>
 
         <Form.Group controlId="formImg">
           <Form.Label>Image URL</Form.Label>
-          <Form.Control type="text" placeholder="Enter image URL" name="img" value={formData.img} onChange={handleChange} />
+          <Form.Control
+          type="text"
+          placeholder="Enter image URL"
+          name="img"
+          value={newProduct.img}
+          onChange={handleChange} />
         </Form.Group>
 
         <Form.Group controlId="formPrice">
           <Form.Label>Price</Form.Label>
-          <Form.Control type="text" placeholder="Enter price" name="price" value={formData.price} onChange={handleChange} />
+          <Form.Control
+          type="text"
+          placeholder="Enter price"
+           name="price"
+          value={newProduct.price}
+          onChange={handleChange} />
         </Form.Group>
 
         <Button className='mt-3' variant="info" type="submit">
