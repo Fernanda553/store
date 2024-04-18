29ebf930ae63d0535715caae18329175a054f288
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-04-18 11:37:27 -0400

    redactoring context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,40 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import StoreContext from './context/StoreContext'
 import { useCart } from './hooks/useCart'
 
 import { PublicRoute, PrivateRoute } from './routes'
 import { OneProduct, Carrito, Home, Login, NewProduct, NotFound, Profile, Register, Thanks } from './views'
 import Navigation from './components/Navigation'
+import { StoreContext } from './context/StoreContext'
 
 const App = (): JSX.Element => {
-  const allState = useCart()
+  const {
+    user,setUser,product,setProduct, isAuthenticated, token, login,
+    logout,
+    products,
+    setProducts,
+    total,
+    setTotal,
+    cart,
+    addCart,
+    setCart,
+    addOneMore,
+    clearCart,
+    removeFromCart } = useCart()
+
+    const allState = {user,setUser,product,setProduct, isAuthenticated, token, login,
+      logout,
+      products,
+      setProducts,
+      total,
+      setTotal,
+      cart,
+      addCart,
+      setCart,
+      addOneMore,
+      clearCart,
+      removeFromCart}
 
   return (
    <StoreContext.Provider value={allState}>
--- frontend/src/context/StoreContext.tsx
+++ frontend/src/context/StoreContext.tsx
@@ -1,6 +1,4 @@
 import { createContext } from 'react'
 import { type PropsUseCart } from '../interfaces/interfaces'
 
-const StoreContext = createContext({} as PropsUseCart)
-
-export default StoreContext
+export const StoreContext = createContext<PropsUseCart>({} as any)
